9d28b9473dc5c27584b6d7f03b9f979073eadd8a
Komal <158317145+Komal0032@users.noreply.github.com>
2025-02-06 10:57:08 +0530

    Add files via upload

---
+++ DateTime.java
@@ -0,0 +1,12 @@
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+public class DateTime{
+    public static void main(String[]args){
+        LocalDateTime current=LocalDateTime.now();
+        System.out.println("current date and time:"+current);
+    }
+
+}
